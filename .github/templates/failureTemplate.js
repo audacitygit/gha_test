2a31d2f0f2110a923b09cb19970209bccecfe4c8
audacitygit <audacitygit@gmail.com>
2025-03-14 07:08:31 -0700

    test: testing jira url

--- .github/templates/failureTemplate.js
+++ .github/templates/failureTemplate.js
@@ -1,7 +1,10 @@
 export function failureMessage(jobName, repo, branch, actor, runUrl) {
-    const ticketNumber = branch.match(/^pr-\d+/) || "pr-01"
-    const ticketUrl = `https://audacitygit.atlassian.net/browse/${ticketNumber}`
+    const jiraBaseUrl = "https://your-jira-instance.atlassian.net/browse/";
+    const ticketId = branch.match(/^pr-\d+/)?.[0]?.toUpperCase() || "UNKNOWN-TICKET";
+    const jiraUrl = `${jiraBaseUrl}${ticketId}`;
+
+
     return {
-        text: `🚨 *${jobName}* Failed in *${repo}* on branch *${branch}* 😭\n🔗 <${runUrl}|View Job>\n🛠 Triggered by: *${actor}* \n view ticket: <${ticketUrl}| Ticket> \n \n INFO: `
+        text: `🚨 *${jobName}* Failed in *${repo}* on branch *${branch}* 😭\n🔗 <${runUrl}|View Job>\n🛠 Triggered by: *${actor}* \n view ticket: <${jiraUrl}| Ticket> \n \n INFO: `
     };
 }
