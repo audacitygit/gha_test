cacf88102dea85e66cc8f683075b2b146ceb3eb8
audacitygit <audacitygit@gmail.com>
2025-03-14 06:41:58 -0700

    testing fake ticket link

--- .github/templates/failureTemplate.js
+++ .github/templates/failureTemplate.js
@@ -1,5 +1,6 @@
 export function failureMessage(jobName, repo, branch, actor, runUrl) {
+    const ticketUrl = "https://audacitygit.atlassian.net/browse/PI-19"
     return {
-        text: `🚨 *${jobName}* Failed in *${repo}* on branch *${branch}* 😭\n🔗 <${runUrl}|View Job>\n🛠 Triggered by: *${actor}*`
+        text: `🚨 *${jobName}* Failed in *${repo}* on branch *${branch}* 😭\n🔗 <${runUrl}|View Job>\n🛠 Triggered by: *${actor}* \n view ticket: <${ticketUrl}| Ticket>`
     };
 }
