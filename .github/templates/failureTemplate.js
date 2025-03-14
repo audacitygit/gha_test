3ce1268be741b3ae596131d71465367535613fd1
audacitygit <audacitygit@gmail.com>
2025-03-14 06:50:51 -0700

    fix: adds branch filtering to failure template

--- .github/templates/failureTemplate.js
+++ .github/templates/failureTemplate.js
@@ -1,5 +1,6 @@
 export function failureMessage(jobName, repo, branch, actor, runUrl) {
-    const ticketUrl = "https://audacitygit.atlassian.net/browse/PI-19"
+    const ticketNumber = branch.match(/^pr-\d+/) || "pr-01"
+    const ticketUrl = `https://audacitygit.atlassian.net/browse/${ticketNumber}`
     return {
         text: `🚨 *${jobName}* Failed in *${repo}* on branch *${branch}* 😭\n🔗 <${runUrl}|View Job>\n🛠 Triggered by: *${actor}* \n view ticket: <${ticketUrl}| Ticket>`
     };
